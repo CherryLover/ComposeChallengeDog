cae78c1793ea180dd46bad1dcd039fa2b2cc7d10
jiangjiwei <jiangjiwei0322@outlook.com>
2021-03-03 00:22:18 +0800

    try fix bug for github workflow

--- app/src/main/java/com/example/androiddevchallenge/DogDetailActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/DogDetailActivity.kt
@@ -1,12 +1,15 @@
 package com.example.androiddevchallenge
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
